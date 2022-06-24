fbc0ed3382bedaad3c041e81c3bc1ab73e17d0d7
hi18yanagi <103158475+yanaginagi39@users.noreply.github.com>
2022-06-24 15:26:44 +0900

    Update Calc.java

--- src/main/java/myapp/Calc.java
+++ src/main/java/myapp/Calc.java
@@ -1,1 +1,18 @@
 
+package myapp;
+
+public class Calc {
+  private int base;
+
+  public Calc(int base){
+    this.base = base;
+  }
+
+  public int plus(int num){
+    return base+num;
+  }
+
+  public int minus(int num){
+    return base-num;
+  }
+}
